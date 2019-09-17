791c071e9fd2e7c0570c33c4f56b136ec0ab2d3e
emmysoft123 <47819331+emmysoft123@users.noreply.github.com>
2019-09-17 14:48:44 -0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,13 @@
+
+function validateForm(){
+      var un = document.loginform.email.value;
+      var pwform = document.loginform.password.value;
+      var username = "username";
+       var pw = "password";
+       if((un == username)  && (pwform == pw)){
+           alert("login was successful.");
+       }else{
+           alert("Login failed");
+       }
+
+}
